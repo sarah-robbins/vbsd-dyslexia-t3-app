ddef27b3d8326c57a00643093d79ac4a4cf18f46
Sarah Robbins <sarah@covalentdesigns.com>
2024-09-15 12:23:58 -0500

    upated version

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,7 @@ const Home: NextPage = () => {
   const [appIsUpdated, setAppIsUpdated] = useState<boolean>(true);
 
   useEffect(() => {
-    const appVersion = "2.1.2 - Beta";
+    const appVersion = "2.2.0 - Beta";
     if (appVersion !== version.toString()) {
       setAppIsUpdated(false);
     }
